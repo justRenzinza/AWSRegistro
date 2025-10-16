bc4d9a412f5a9d652d219bb326960a5f5d4ba2c7
justRenzinza <renzogavance@gmail.com>
2025-10-16 16:58:10 -0300

    no-explicit-any

--- app/helpers/search.ts
+++ app/helpers/search.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 //search generico pra funcionar tanto no cliente, tanto no controle-sistema
 function toStr(v: unknown): string {
 	return String(v ?? "").trim().toLowerCase();
